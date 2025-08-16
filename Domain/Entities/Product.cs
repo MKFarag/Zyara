f60c5f6b756e43de97580451d501e460ef3f734c
MKFarag <mohammedkhaled.fa@gmail.com>
2025-08-16 09:51:05 +0300

    Change column name in product

--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -6,7 +6,7 @@ public class Product
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int StorageQuantity { get; set; }
-    public float BuyingPrice { get; set; }
+    public float CurrentPrice { get; set; }
     public float SellingPrice { get; set; }
 
     // TODO: Add ProductImage table and its navigation property
---
+++ Infrastructure/Persistence/Migrations/20250816065007_ChangeColumnNameInProduct.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeColumnNameInProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BuyingPrice",
+                table: "Products",
+                newName: "CurrentPrice");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CurrentPrice",
+                table: "Products",
+                newName: "BuyingPrice");
+        }
+    }
+}
