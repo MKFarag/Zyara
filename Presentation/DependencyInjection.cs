f2d3b9e4ce5518eea20f376b038d8992cfa57344
MKFarag <mohammedkhaled.fa@gmail.com>
2025-08-29 07:52:57 +0300

    Add project files.

---
+++ Presentation/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace Presentation;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+
+
+
+        services
+            .AddEndpointsApiExplorer()
+            .AddOpenApi();
+
+        return services;
+    }
+}
---
+++ Presentation/Program.cs
@@ -0,0 +1,25 @@
+#region Usings
+
+using Presentation; 
+
+#endregion
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDependencies(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
